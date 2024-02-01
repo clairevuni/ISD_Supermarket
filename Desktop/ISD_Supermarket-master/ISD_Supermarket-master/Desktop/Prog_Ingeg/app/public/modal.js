c7e9a116b662efa3b4989906e57a46494bb007aa
Chiaraunict <LAICHR01E70I754N@studium.unict.it>
2024-02-01 17:37:21 +0100

    aggiunto css della model

--- Desktop/ISD_Supermarket-master/ISD_Supermarket-master/Desktop/Prog_Ingeg/app/public/modal.js
+++ Desktop/ISD_Supermarket-master/ISD_Supermarket-master/Desktop/Prog_Ingeg/app/public/modal.js
@@ -1,14 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const openModalButton = document.getElementById("openModalButton");
-    const modal = document.getElementById("myModal");
-  
-    openModalButton.addEventListener("click", function () {
-      modal.style.display = "block";
-    });
-  
-    window.addEventListener("click", function (event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    });
+  const openModalButton = document.getElementById("openModalButton");
+  const modal = document.getElementById("myModal");
+
+  openModalButton.addEventListener("click", function () {
+    modal.style.display = "block";
+    modal.classList.add("small-modal"); // Aggiungi la classe per la modal più piccola
   });
+
+  // Chiudi la modal cliccando fuori o sull'icona "x"
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  // Chiudi la modal cliccando sull'icona "x"
+  const closeIcon = document.createElement("span");
+  closeIcon.innerHTML = "&times;"; // Carattere "x"
+  closeIcon.classList.add("close-icon");
+  modal.appendChild(closeIcon);
+
+  closeIcon.addEventListener("click", closeModal);
+
+  // Funzione per chiudere la modal
+  function closeModal() {
+    modal.style.display = "none";
+    modal.classList.remove("small-modal"); // Rimuovi la classe per la modal più piccola
+  }
+});
